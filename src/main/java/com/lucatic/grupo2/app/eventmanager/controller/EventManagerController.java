ca1c568236cd39b46e56e198d6f46da2c777af42
Daniel <dangimrod@gmail.com>
2024-03-20 11:04:20 +0100

    Swagger controller

--- src/main/java/com/lucatic/grupo2/app/eventmanager/controller/EventManagerController.java
+++ src/main/java/com/lucatic/grupo2/app/eventmanager/controller/EventManagerController.java
@@ -4,6 +4,12 @@ import com.lucatic.grupo2.app.eventmanager.exceptions.CheckEventUserExistExcepti
 import com.lucatic.grupo2.app.eventmanager.models.dto.BoolResponseWithError;
 import com.lucatic.grupo2.app.eventmanager.models.dto.StringResponseWithError;
 import com.lucatic.grupo2.app.eventmanager.service.EventManagerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,23 +28,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/eventmanager")
 public class EventManagerController {
 
-    @Autowired
-    private EventManagerService eventManagerService;
+	@Autowired
+	private EventManagerService eventManagerService;
 
-    @GetMapping("/checkExist/{idUser}/{idEvent}")
-    public ResponseEntity<?> checkUserEvent(@PathVariable Long idUser, @PathVariable Long idEvent) throws CheckEventUserExistException {
-        BoolResponseWithError boolResponseWithError = new BoolResponseWithError();
-        if ((eventManagerService.checkUserEvent(idUser, idEvent))) {
-            boolResponseWithError.setRespBool(true);
-        } else {
-            boolResponseWithError.setErrorBool(false);
-        }
-        boolResponseWithError.setError(null);
-        boolResponseWithError.setErrorBool(false);
-        return ResponseEntity.ok(boolResponseWithError);
-    }
+	@Operation(summary = "Comprueba la existencia de un usuario y evento por id", description = "Devuelve la existencia de un usuario y evento", tags = {
+			"user" })
+	@ApiResponses(value = {
+			@ApiResponse(description = "Comprueba usuario y evento", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = BoolResponseWithError.class))),
+			@ApiResponse(responseCode = "404", description = "No hay usuarios y/o eventos con ese id", content = @Content),
+			@ApiResponse(responseCode = "500", description = "Error genérico mostrando evento y/o usuario", content = @Content)
 
+	})
+	@GetMapping("/checkExist/{idUser}/{idEvent}")
+	public ResponseEntity<?> checkUserEvent(@PathVariable Long idUser, @PathVariable Long idEvent)
+			throws CheckEventUserExistException {
+		BoolResponseWithError boolResponseWithError = new BoolResponseWithError();
+		if ((eventManagerService.checkUserEvent(idUser, idEvent))) {
+			boolResponseWithError.setRespBool(true);
+		} else {
+			boolResponseWithError.setErrorBool(false);
+		}
+		boolResponseWithError.setError(null);
+		boolResponseWithError.setErrorBool(false);
+		return ResponseEntity.ok(boolResponseWithError);
+	}
 
-    @GetMapping("/eventmanager/getUser/{idUser}")
-    public StringResponseWithError getNameUser(@PathVariable Long idUser);
+	@Operation(summary = "Comprueba la existencia de un usuario por id", description = "Devuelve la existencia de un usuario", tags = {
+			"user" })
+	@ApiResponses(value = {
+			@ApiResponse(description = "Comprueba un usuario", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = StringResponseWithError.class))),
+			@ApiResponse(responseCode = "404", description = "No hay usuario con ese id", content = @Content),
+			@ApiResponse(responseCode = "500", description = "Error genérico mostrando usuario", content = @Content)
+
+	})
+	@GetMapping("/getUser/{idUser}")
+	public ResponseEntity<?> getNameUser(@PathVariable Long idUser) {
+		StringResponseWithError stringResponseWithError = eventManagerService.getNameUser(idUser);
+		stringResponseWithError.setError(null);
+		stringResponseWithError.setErrorBool(false);
+		return ResponseEntity.ok().body(stringResponseWithError);
+	}
 }
